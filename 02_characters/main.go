7c1f63abedd1d0416c3a943800d8211cb33bdecb
kiknaio <g.kiknadze@hotmail.com>
2018-08-06 22:27:36 +0400

    Add loop diferent numeric types

--- 02_characters/main.go
+++ 02_characters/main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Printf("%d - %b - %x\n", 42, 42, 42)
+	// Print numbers in decimal, binary and hexidecimal
+	for i:= 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t 0x%x \n", i, i, i)
+	}
 }
