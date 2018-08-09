f6d93468ceefbef395e5b6ba0920fc3578b3b8b6
kiknaio <g.kiknadze@hotmail.com>
2018-08-09 23:33:50 +0400

    add comments to pointers

--- 09_memory_management/pointers.go
+++ 09_memory_management/pointers.go
@@ -2,12 +2,16 @@ package main
 
 // Pointer looks to memory address and changes the value of
 // that memory address
+
+// With * we are telling to modify the original value
+// by getting the memory address
 func zero(z *int)  {
 	*z = 0
 }
 
 func main() {
 	x := 5
+	// & finds the memory address. Where it is stored
 	zero(&x)
 	println(x) // 0
 }
--- 10_remainders/main.go
+++ 10_remainders/main.go
@@ -6,21 +6,21 @@ import (
 )
 
 func main() {
-	//x := 13 % 3
-	//
-	//if x == 1 {
-	//	fmt.Println("Odd")
-	//} else {
-	//	fmt.Println("Event")
-	//}
-	//
-	//for i := 0; i < 100; i++ {
-	//	if i % 2 == 1 {
-	//		fmt.Println("Odd")
-	//	} else {
-	//		fmt.Println("Event")
-	//	}
-	//}
+	x := 13 % 3
+
+	if x == 1 {
+		fmt.Println("Odd")
+	} else {
+		fmt.Println("Event")
+	}
+
+	for i := 0; i < 100; i++ {
+		if i % 2 == 1 {
+			fmt.Println("Odd")
+		} else {
+			fmt.Println("Event")
+		}
+	}
 
 	fmt.Println(math.Sqrt(7))
 }
