e2db0f8023f18ec9adca2b1a037025e8bd9f3b6e
kiknaio <g.kiknadze@hotmail.com>
2018-08-19 18:59:22 +0200

    slice literals

---
+++ 24_slices/02_initialize_slices/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	mySlice := make([]string, 35)
+	mySecondSlice := []string{}
+
+	// Slice literals
+	fmt.Println(mySecondSlice)
+	fmt.Println(mySlice)
+}
