ca7b78419041fae3c74bfc6887d435d4ae377d60
kiknaio <g.kiknadze@hotmail.com>
2018-09-01 14:26:46 +0200

    Mutex example

---
+++ 34_concurrency/03_mutex/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+var counter int
+var mutex sync.Mutex
+
+func increment(s string)  {
+	for i := 0; i < 20; i++ {
+		time.Sleep(time.Duration(rand.Intn(20)) * time.Millisecond)
+		mutex.Lock()
+		counter++
+		fmt.Println(s, i, "Counter:", counter)
+		mutex.Unlock()
+	}
+	wg.Done()
+}
+
+func main() {
+	wg.Add(2)
+	go increment("Foo:")
+	go increment("Bar:")
+	wg.Wait()
+	fmt.Println("Final counter:", counter)
+}
